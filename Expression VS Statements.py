8f392560eef9e6808b7e2bb57eaa5c5f7ec04474
Rahul Bhardwaj <98464340+FreaPK@users.noreply.github.com>
2026-01-01 14:59:39 +0530

    Add files via upload

---
+++ Expression VS Statements.py
@@ -0,0 +1,6 @@
+#Expressions vs Statements
+#a statement is a entire line of code that performs an action
+iq=200#This is also a statement
+user_age=iq/2#This line of code is a statement and #iq/2 is a expression
+#iq/2 is a part of the statement
+print(user_age)
